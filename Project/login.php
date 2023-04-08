d90b3e4a0785602340b9747ffb3a31003b3da813
AlexUkraineAUE <krosbond@gmail.com>
2023-04-07 19:30:45 -0500

    Login added to all pages

--- Project/login.php
+++ Project/login.php
@@ -2,7 +2,7 @@
 require('connect.php');
 session_start();
 
-if(isset($_POST['username']) && isset($_POST['password'])) {
+if(!empty($_POST['username']) && !empty($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -14,56 +14,53 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
 
     $user = $statement->fetch();
 
-        if($user) {
-            
-            $_SESSION['user'] = $user; 
-            header("Location: index.php");
-            exit();
-        } 
+    if($user) {
+        $_SESSION['user'] = $user; 
         header("Location: index.php");
         exit();
     } else {
-        // login failed, show error message
         $error = "Invalid username or password";
     }
+}
 
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login - Winnipeg Telecom</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
-<body>
-    <header>
-        <h1>Winnipeg Telecom</h1>
-    </header>
-    <nav>
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About Us</a></li>
-            <li><a href="services.php">Services</a></li>
-            <li><a href="contact.php">Contact Us</a></li>
-        </ul>
-    </nav>
-    <main>
-        <h2>Login</h2>
-        <?php if(isset($error)): ?>
-            <p class="error"><?= $error ?></p>
-        <?php endif; ?>
-        <form method="post" action="login.php">
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username">
-            <br>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password">
-            <br>
-            <input type="submit" value="Login">
-        </form>
-    </main>
-    <footer>
-        <p>&copy; 2023 Winnipeg Telecom. All rights are sold separately.</p>
-    </footer>
-</body>
-</html>
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Login - Winnipeg Telecom</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <header>
+            <h1>Winnipeg Telecom</h1>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+        </nav>
+        <main>
+            <h2>Login</h2>
+            <?php if(isset($error)): ?>
+                <p class="error"><?= $error ?></p>
+            <?php endif; ?>
+            <form method="post" action="login.php">
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username">
+                <br>
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+                <br>
+                <input type="submit" value="Login">
+            </form>
+        </main>
+        <footer>
+            <p>&copy; 2023 Winnipeg Telecom. All rights are sold separately.</p>
+        </footer>
+    </body>
+    </html>
